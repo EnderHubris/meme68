bfea6bd57d9530408fe74b7c159d7236d0149ebb
Ender <nallahabi2003@gmail.com>
2026-06-18 19:45:42 -0400

    Initial dokploy rework

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,5 @@ while [ ! -S /var/run/supervisor.sock ]; do
     sleep 0.1
 done
 
-# Reload nginx if certs exist
-if [ -f /etc/letsencrypt/live/meme68.com/fullchain.pem ] && [ -f /etc/letsencrypt/live/meme68.com/privkey.pem ]; then
-    echo "Reloading NGINX with LetsEncrypt certs..."
-    supervisorctl restart nginx
-fi
-
-# ensure certbot challenge directory is present and modifiable by www-data
-mkdir -p /var/www/certbot/.well-known/acme-challenge
-chown -R www-data:www-data /var/www/certbot
-
 # Keep container alive
 wait
